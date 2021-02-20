c84b2759624fac2aaa09f9f1d2c2449662e665ef
Md Saiful Alam <bm.bcpaul@gmail.comm>
2021-02-20 20:38:57 +0600

    get and post to server

---
+++ scope.js
@@ -0,0 +1,20 @@
+
+var hoistingTest = "Hoisting Test"
+
+function hosting (){
+
+    var hoistingTest='Inside Hosting of function';
+    
+    console.log (hoistingTest);
+
+    
+}
+
+hosting();
+
+var numbers = [2, 3, 4, 5, 6, 7 ,8 ,9 , 10, 11]
+
+var slice = numbers.splice(2,6);
+
+console.log(slice);
+console.log(numbers);
